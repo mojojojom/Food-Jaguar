d2965e19c4e887ae16caa3f125986375609b53fc
Jommel Dalisaymo <dalisaymo36@gmail.com>
2023-01-16 09:40:31 +0800

    Initial Commit

--- DATABASE FILE/foodonline_db.sql
+++ DATABASE FILE/foodonline_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 14, 2023 at 03:34 PM
+-- Generation Time: Jan 16, 2023 at 01:39 AM
 -- Server version: 5.7.33
 -- PHP Version: 7.4.19
 
@@ -41,7 +41,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`adm_id`, `username`, `password`, `email`, `code`, `date`) VALUES
-(1, 'admin', 'CAC29D7A34687EB14B37068EE4708E7B', 'admin@mail.com', '', '2022-05-27 13:21:52');
+(1, 'admin', 'CAC29D7A34687EB14B37068EE4708E7B', 'admin@mail.com', '', '2022-05-27 13:21:52'),
+(2, 'foodjaguar', '$2y$10$BO/COk9RVY2jm5tCH3sT.uZxRwgXJvQMqgfOwIHCg6J5/0v18U5LO', 'foodjaguar@gmail.com', '12345', '2023-01-15 17:28:57');
 
 -- --------------------------------------------------------
 
@@ -139,37 +140,6 @@ INSERT INTO `remark` (`id`, `frm_id`, `status`, `remark`, `remarkDate`) VALUES
 (12, 5, 'closed', 'none', '2022-05-27 13:18:03'),
 (13, 47, 'in process', 'thank you for ordering!', '2023-01-14 01:32:11'),
 (14, 47, 'closed', 'Thank you for ordering!', '2023-01-14 05:15:08');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `restaurant`
---
-
-CREATE TABLE `restaurant` (
-  `rs_id` int(222) NOT NULL,
-  `c_id` int(222) NOT NULL,
-  `title` varchar(222) NOT NULL,
-  `email` varchar(222) NOT NULL,
-  `phone` varchar(222) NOT NULL,
-  `url` varchar(222) NOT NULL,
-  `o_hr` varchar(222) NOT NULL,
-  `c_hr` varchar(222) NOT NULL,
-  `o_days` varchar(222) NOT NULL,
-  `address` text NOT NULL,
-  `image` text NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `restaurant`
---
-
-INSERT INTO `restaurant` (`rs_id`, `c_id`, `title`, `email`, `phone`, `url`, `o_hr`, `c_hr`, `o_days`, `address`, `image`, `date`) VALUES
-(1, 1, 'North Street Tavern', 'nthavern@mail.com', '3547854700', 'www.northstreettavern.com', '8am', '8pm', 'mon-sat', '1128 North St, White Plains', '6290877b473ce.jpg', '2022-05-27 08:10:35'),
-(2, 2, 'Eataly', 'eataly@gmail.com', '0557426406', 'www.eataly.com', '11am', '9pm', 'Mon-Sat', '800 Boylston St, Boston', '606d720b5fc71.jpg', '2022-05-27 08:06:41'),
-(3, 3, 'Nan Xiang Xiao Long Bao', 'nanxiangbao45@mail.com', '1458745855', 'www.nanxiangbao45.com', '9am', '8pm', 'mon-sat', 'Queens, New York', '6290860e72d1e.jpg', '2022-05-27 08:04:30'),
-(4, 4, 'Highlands Bar & Grill', 'hbg@mail.com', '6545687458', 'www.hbg.com', '7am', '8pm', 'mon-sat', '812 Walter Street', '6290af6f81887.jpg', '2022-05-27 11:01:03');
 
 -- --------------------------------------------------------
 
@@ -336,12 +306,6 @@ ALTER TABLE `remark`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `restaurant`
---
-ALTER TABLE `restaurant`
-  ADD PRIMARY KEY (`rs_id`);
-
---
 -- Indexes for table `res_category`
 --
 ALTER TABLE `res_category`
@@ -379,7 +343,7 @@ ALTER TABLE `user_order_summary`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `adm_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `adm_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `dishes`
@@ -398,12 +362,6 @@ ALTER TABLE `food_category`
 --
 ALTER TABLE `remark`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- AUTO_INCREMENT for table `restaurant`
---
-ALTER TABLE `restaurant`
-  MODIFY `rs_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `res_category`
